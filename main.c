8a1c95c08067d0f275351a166edae4a7867790a3
nellepot <mutambolero@up.edu.ph>
2026-02-10 03:28:34 +0800

    feat: implementd cd and pwd in-built commands

--- main.c
+++ main.c
@@ -22,9 +22,31 @@ int main(){
         
         Command cmd = parse_input(input);
 
-        if (strcmp(cmd.args[0], "exit") == 0) {
+        if (strcmp(cmd.command, "exit") == 0) {
             break;                            
         }
+
+        if (strcmp(cmd.command, "cd") == 0) {
+            if (cmd.args[1] == NULL) {
+                fprintf(stderr, "mysh: expected argument to \"cd\"\n");
+            } else {
+                if (chdir(cmd.args[1]) != 0) {
+                    perror("mysh");
+                }
+            }
+            continue;                                                   // Handle built-in 'cd' and skip to next loop
+        }
+
+        if(strcmp(cmd.command, "pwd") == 0){
+            char cwd[1024];
+            if (getcwd(cwd, sizeof(cwd)) != NULL) {
+                printf("%s\n", cwd);
+            } else {
+                perror("mysh");
+            }
+            continue;                                                   // Handle built-in 'pwd' and skip to next loop
+        }
+        
     pid_t pid = fork();                                                 // External Command Execution
 
     if (pid == 0) {
